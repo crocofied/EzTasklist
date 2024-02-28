e666beb590cf19f6f07ffe4b3cec96f3c91d6b3d
DamiansNase <damian.bergemann.privat@gmail.com>
2024-02-28 21:59:16 +0100

    Started with UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,14 @@ function App() {
 
   return (
     <>
-      <h1>App</h1>
+      <div className='dark'>
+        <div className='dark:bg-gray-900 bg-white h-screen flex flex-col items-center'>
+          <h1 className='dark:text-white text-black text-3xl font-bold p-2'>EzTasklist</h1>
+          <div className='border dark:border-white border-black w-2/3 lg:w-2/5'>
+            
+          </div>
+        </div>
+      </div>
     </>
   )
 }
